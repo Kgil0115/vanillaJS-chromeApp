e68fdee60544fb324b3f97d02aefc0b0e0f6842c
kgil0115 <gilwon0115@gmail.com>
2021-02-21 16:24:46 +0900

    develop chrome app

---
+++ todo.js
@@ -0,0 +1,78 @@
+const toDoForm = document.querySelector(".js-toDoForm"),
+  toDoInput = toDoForm.querySelector("input"),
+  toDoList = document.querySelector(".js-toDoList");
+
+const TODOS_LS= 'toDos';
+let toDos = [];
+
+function filterFn(toDo){
+  return toDo.id === 1
+}
+
+function deleteToDo(event){
+  //console.dir(event.target.parentNode);
+  const btn = event.target;
+  const li = btn.parentNode;
+  console.log(li);
+  toDoList.removeChild(li);
+  const cleanToDos = toDos.filter(function(toDo){
+    return toDo.id !== parseInt(li.id)
+  });
+  toDos = cleanToDos;
+  saveToDos();
+}
+
+function saveToDos(){
+  localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
+}
+
+function paintToDo(text){
+  //console.log(text);
+  const li = document.createElement("li");
+  const delBtn = document.createElement("button");
+  const span = document.createElement("span");
+  //const newId = toDos.length +1;
+  const newId = toDos.length>0 ? (toDos[toDos.length-1].id + 1) : 1;
+
+  delBtn.innerText = "🆇";
+  delBtn.addEventListener("click", deleteToDo);
+  span.innerHTML = text;
+  li.appendChild(delBtn);
+  li.appendChild(span);
+  li.id = newId;
+
+  toDoList.appendChild(li);
+  
+  const toDoObj = {
+    text: text,
+    id: newId
+  };
+  toDos.push(toDoObj);
+  saveToDos();
+}
+
+function handleSubmit(event){
+  event.preventDefault();
+  const currentValue = toDoInput.value;
+  paintToDo(currentValue);
+  toDoInput.value = "";
+}
+
+function loadToDos(){
+  const loadedToDos = localStorage.getItem(TODOS_LS);
+  if(loadedToDos !== null){
+    const parsedToDos = JSON.parse(loadedToDos);
+    parsedToDos.forEach(function(toDo){
+      console.log(toDo.text);
+      paintToDo(toDo.text);
+    });
+  } 
+  
+}
+
+  function init(){
+    loadToDos();
+    toDoForm.addEventListener("submit", handleSubmit);
+  }
+
+  init();
